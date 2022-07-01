ba60d8b6d16d0730ac0fdab190523412b0ccdf64
Duc Nguyen Huu <intern.nhduc3@tma.com.vn>
2022-07-01 14:31:41 +0700

    setup co ban cho router va vuex

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  Detail for coaches
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <section><h1>Filter</h1></section>
+    <section>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+            {{ coach.firstName }}
+        </li>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
+};
+</script>
+
+<style>
+</style>
